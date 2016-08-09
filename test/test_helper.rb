b8970bf5ad05f0d4fb16e5e474832e2912d2310c
attackontitan <letsgogogo321go@gmail.com>
2016-08-09 12:53:12 -0500

    still have questions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,22 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   def is_logged_in?
-    puts "2222222222"
-    puts @session
-    @session[:user_id].nil?
+
+    !session[:user_id].nil?
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path,   session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } 
   end
 end
